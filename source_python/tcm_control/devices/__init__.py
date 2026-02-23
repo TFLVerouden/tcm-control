7960d34548f29c4c389b9843e83994757bb1aa3f
Tommie Verouden <t.f.l.verouden@utwente.nl>
2026-02-23 09:33:44 +0100

    Initialise syringe pump too

--- source_python/tcm_control/devices/__init__.py
+++ source_python/tcm_control/devices/__init__.py
@@ -1,5 +1,6 @@
 from .base import PoFSerialDevice
 from .cough_machine import CoughMachine
 from .spraytec_lift import SprayTecLift
+from .syringe_pump import SyringePump
 
-__all__ = ["PoFSerialDevice", "CoughMachine", "SprayTecLift"]
+__all__ = ["PoFSerialDevice", "CoughMachine", "SprayTecLift", "SyringePump"]
