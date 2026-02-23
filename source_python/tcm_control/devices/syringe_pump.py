e222b50ec20e706fe9185f8102b392d952eed335
Tommie Verouden <t.f.l.verouden@utwente.nl>
2026-02-23 16:16:55 +0100

    Added syringe picker based on volume (TO BE TESTED)

--- source_python/tcm_control/devices/syringe_pump.py
+++ source_python/tcm_control/devices/syringe_pump.py
@@ -1,6 +1,8 @@
 import pumpy3
 import time
-from tcm_utils.io_utils import prompt_input
+from pathlib import Path
+from tcm_utils.file_dialogs import read_repo_config_value, write_repo_config_value
+from tcm_utils.io_utils import prompt_input, load_two_column_numeric
 
 # Source code and documentation: https://github.com/Wetenschaap/pumpy3
 # First ensure RS-232 settings on the PHD 2000 are configured.
@@ -40,6 +42,36 @@ class SyringePump(pumpy3.PumpPHD2000_Refill):
         # Initialise PHD 2000
         super().__init__(chain, address=pump_address, name="PHD2000")
         self.set_mode("PMP")  # Set to PuMP mode
+        self.set_diameter(diameter_mm)
+
+    # TODO: TEST BELOW FUNCTIONS
+    @staticmethod
+    def get_syringe_diameter(
+        volume_ml: float,
+        type: str = "hamilton_microliter_gastight",
+        lookup_table_path: str | Path = "config/syringe_sizes.csv",
+    ) -> float:
+        # From the look-up table for in the PHD 2000 manual, as mirrored
+        # in /config/syringe_sizes.csv, get the diameter for a syringe of the
+        # given volume and type.
+
+        # Only support relevant type
+        if type != "hamilton_microliter_gastight":
+            raise NotImplementedError(f"Syringe type {type} not implemented.")
+
+        # Read the look-up table and find the diameter for the given volume
+        table = load_two_column_numeric(Path(lookup_table_path))
+        for row in table:
+            row_volume, row_diameter = row
+            if row_volume == volume_ml:
+                return row_diameter
+
+        # Else, if no match found, raise an error
+        raise ValueError(
+            f"No diameter found for syringe type {type} and volume {volume_ml} mL.")
+
+    def set_syringe_volume(self, volume_ml: float, type: str = "hamilton_microliter_gastight"):
+        diameter_mm = self.get_syringe_diameter(volume_ml, type=type)
         self.set_diameter(diameter_mm)
 
 
