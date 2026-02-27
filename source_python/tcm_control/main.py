40f874cbc0dd7bf274d3b34b1de67c6f27289f12
Tommie Verouden <44117924+TFLVerouden@users.noreply.github.com>
2026-02-27 11:54:54 +0100

    Test droplet counting

--- source_python/tcm_control/main.py
+++ source_python/tcm_control/main.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 from importlib import resources
+import time
 
-from tcm_control.devices import CoughMachine, SprayTecLift
+from tcm_control.devices import CoughMachine, SprayTecLift, SyringePump
 from tcm_control import logger
 from tcm_utils.io_utils import prompt_input
 
@@ -22,14 +23,30 @@ def ask_user_for_comments(experiment_dir: Path) -> str:
 if __name__ == "__main__":
     # Config variables
     FLOW_CURVE_CSV_PATH = None
+    SYRINGE_VOLUME_ML = 2.5
+    TANK_PRESSURE_BAR = 1.5
+    DROPLET_PUMP_RATE_ML_PER_MIN = 0.1
 
+    # Initialise devices
     lift = SprayTecLift()
-    print("Current height:", lift.get_height(), " mm")
-    # lift.read_status(echo=True)
-    # cough_machine = CoughMachine(debug=False)
-    # cough_machine.clear_memory()
-    # cough_machine.set_pressure(1.5, timeout_s=10.0)
+    # print("Current height:", lift.get_height(), " mm")
+    pump = SyringePump(syringe_volume_ml=SYRINGE_VOLUME_ML)
+    tcm = CoughMachine(debug=False)
 
-    # cough_machine.load_flowcurve(csv_path="step")
-    # cough_machine.detect_droplet(runs=2, output_dir=output_dir)
-    # cough_machine.manual_mode()
+    # Cough machine settings
+    tcm.set_pressure(TANK_PRESSURE_BAR, timeout_s=10.0)
+    tcm.load_flowcurve(csv_path="step")
+
+    # Turn on syringe pump
+    pump.infuse(pump_rate_ml_mn=DROPLET_PUMP_RATE_ML_PER_MIN)
+    time.sleep(2)  # Wait a bit for the pump to start infusing
+
+    # Go into droplet detection mode with a finite count
+    detections = tcm.count_droplets(runs=5)
+    print(f"Detected droplets: {detections}")
+
+    # Ensure active modes are stopped
+    tcm.quit()
+
+    # Stop the pump after droplet detection is done
+    pump.stop()
