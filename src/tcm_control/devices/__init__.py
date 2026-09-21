671359e2fa200bf096c31e61b098193abf653f44
Tommie Verouden <44117924+TFLVerouden@users.noreply.github.com>
2026-09-21 11:18:55 +0200

    Removed syringe pump 1

--- src/tcm_control/devices/__init__.py
+++ src/tcm_control/devices/__init__.py
@@ -1,10 +1,10 @@
 from .base import PoFSerialDevice
 from .cough_machine import CoughMachine
 from .vertical_stage import VerticalStage
-from .syringe_pump import SyringePump
+# from .syringe_pump import SyringePump
 from .syringe_pump2 import SyringePump2
 from .spraytec import SprayTec
 from .camera import Camera
 
 __all__ = ["PoFSerialDevice", "CoughMachine", "VerticalStage",
-           "SyringePump", "SprayTec", "Camera", "SyringePump2"]
+           "SprayTec", "Camera", "SyringePump2"]
